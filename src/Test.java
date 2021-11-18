2956e8fdbfd488e0a4126ca181c51273b783cad4
[邵荣宗 <2636800985@qq.com>
2021-11-18 15:20:39 +0800

    Revert "使用jna，将C里面的getch函数拿来用，以实现键盘监听（人机交互步骤）"
    This reverts commit 2c97cdd7

--- src/Test.java
+++ src/Test.java
@@ -1,33 +1,19 @@
 import Bricks.Core;
+import threds.Handing;
 import threds.Print;
-import Test.Mydll;
 
 ///程序的入口
 public class Test {
     public static void main(String[] args){
         Core core = new Core();
         Print print = new Print();
+        //Handing handing = new Handing();
         Thread printThread = new Thread(print);
         Thread handingThread = new Thread(print);
         printThread.setName("打印");
         handingThread.setName("搬运");
-        handingThread.start();
         printThread.start();
-        while (true){
-            try{
-                int a = Mydll.mydll.qiguai_1();
-                if(a == 100){
-                    Core.jihe[1] += 1;
-                    Core.jihe[3] += 1;
-                    Core.jihe[5] += 1;
-                    Core.jihe[7] += 1;
-                }
-            }
-            catch ( Exception e){
-
-            }
-        }
-
+        handingThread.start();
 
     }
 
